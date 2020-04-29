a0752533f1b66c34ded708a1224d68a912ab9e28
Jacky Jiang <t83714@gmail.com>
2020-04-29 11:06:49 +1000

    implement the function logic for processing ESRI API

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,96 @@
-export default async function myFunction(input: any) {
-    return "Hello world!\n";
+import axios from "axios";
+import URI from "urijs";
+import isUrl from "is-url";
+import { Record } from "@magda/registry-client";
+
+export type UrlProcessorResult = {
+    dataset: Record;
+    distributions: Record[];
+};
+
+const isNumber = (v: any) => typeof v === "number";
+
+function getSpatialCoverageAspect(data: any) {
+    if (
+        !isNumber(data?.extent?.xmax) ||
+        !isNumber(data?.extent?.ymax) ||
+        !isNumber(data?.extent?.xmin) ||
+        !isNumber(data?.extent?.ymin)
+    ) {
+        return null;
+    }
+    return {
+        // -- Bounding box in order minlon (west), minlat (south), maxlon (east), maxlat (north)
+        bbox: [
+            data.extent.xmin,
+            data.extent.ymin,
+            data.extent.xmax,
+            data.extent.ymax
+        ],
+        spatialDataInputMethod: "bbox"
+    };
+}
+
+export default async function myFunction(
+    input: any
+): Promise<UrlProcessorResult> {
+    if (!isUrl(input)) {
+        throw new Error("Expect the input to be an valid URL.");
+    }
+    const uri = URI(input);
+    const query = uri.search(true);
+    if (query?.f != "json") {
+        uri.search({ ...query, f: "json" });
+    }
+
+    const url = uri.toString();
+    const res = await axios.get(uri.toString());
+    if (res.status !== 200) {
+        throw new Error(
+            `Failed to retrieve data from url : ${url} Error: ${res.statusText}`
+        );
+    }
+
+    if (!res?.data?.id || !res?.data?.name) {
+        throw new Error(
+            `Failed to retrieve data from esri API: ${url} Error: can't locate id or name`
+        );
+    }
+
+    const data = res.data;
+    const spatialCoverageAspect = getSpatialCoverageAspect(data);
+
+    const result: UrlProcessorResult = {
+        dataset: {
+            id: "",
+            name: "",
+            sourceTag: "",
+            tenantId: 0,
+            aspects: {}
+        },
+        distributions: [
+            {
+                id: data.id,
+                name: data.name,
+                sourceTag: "",
+                tenantId: 0,
+                aspects: {
+                    "dcat-distribution-strings": {
+                        title: data.name,
+                        description: data.description,
+                        format: "ESRI REST"
+                    }
+                }
+            }
+        ]
+    };
+
+    if (spatialCoverageAspect) {
+        result.dataset.aspects["spatial-coverage"] = spatialCoverageAspect;
+        result.distributions[0].aspects[
+            "spatial-coverage"
+        ] = spatialCoverageAspect;
+    }
+
+    return result;
 }
--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -1,10 +1,48 @@
 import { expect } from "chai";
 import "mocha";
+import nock, { Scope } from "nock";
+import { default as esriSampleResData } from "./esri-sample.json";
+import { default as resultSampleResData } from "./result-sample.json";
 import myFunction from "../index";
 
 describe("Test Function", () => {
-    it("should return 'Hello world!\n'", async () => {
-        const result = await myFunction(undefined);
-        expect(result).to.equal("Hello world!\n");
+    let esriApiScope: Scope;
+    const esriApiUrl = "http://example.com";
+
+    after(() => {
+        nock.cleanAll();
+        nock.enableNetConnect();
+    });
+
+    before(() => {
+        nock.disableNetConnect();
+        // Allow localhost connections so we can test local routes and mock servers.
+        nock.enableNetConnect("127.0.0.1");
+
+        esriApiScope = nock(esriApiUrl).persist();
+
+        esriApiScope
+            .get("/")
+            .query(query => {
+                return query?.f === "json";
+            })
+            .reply(200, esriSampleResData);
+
+        esriApiScope
+            .get("/")
+            .query(query => {
+                return query?.f !== "json";
+            })
+            .reply(200, "This is a html page of the API.");
+    });
+
+    it("should extract correct metadata for url WITH ?f=json", async () => {
+        const result = await myFunction("http://example.com/?f=json");
+        expect(result).to.deep.equal(resultSampleResData);
+    });
+
+    it("should extract correct metadata for url WITHOUT ?f=json", async () => {
+        const result = await myFunction("http://example.com/");
+        expect(result).to.deep.equal(resultSampleResData);
     });
 });
